22d3269776065f3043fccf02b3d81fad8d6cc15e
Программист 1С <1cdev@resurs.sbk>
2023-03-17 09:51:18 +0300

    full project

--- src/components/headerHead.vue
+++ src/components/headerHead.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="generalHead">
-        <section class="section space-around">
+        <section class="section header">
+            <div class="space-around content">
             <form action="" class="form wrapper-col">
                 <span class="wrapper space-around tabs" >
                     <span class="tab" @click="formTabs()" :class="{active2: isActive2}">Написать нам</span>
@@ -8,16 +9,31 @@
                 </span>
                 <input class="input" type="text" name="name" placeholder="Имя">
                 <input class="input" type="number" name="tel" placeholder="Номер телефона">
-                <input class="input" type="email" name="email" placeholder="Электронная почта">
-                <textarea class="textarea" name="message">Сообщение</textarea>
+                <input class="input" type="email" name="email" :class="{active: isActive2}" placeholder="Электронная почта">
+                <textarea class="textarea" :class="{active: isActive2}" name="message">Сообщение</textarea>
                 <button class="btn" type="submit">Отправить</button>
             </form>
             <div class="get wrapper-col">
                 <h1 class="h1">Более 10 лет на рынке</h1>
                 <h4 class="h4">Комфортно, безопасно и в нужное время доставим Вас до любой точки города </h4>
-                <button type="submit"><h3 class="h3"><p class="p">Заказать такси</p></h3></button>
+                <button type="submit" class="takeCar"><h3 class="h3"><p class="p">Заказать автомобиль</p></h3></button>
             </div>
+        </div>
         </section>
+        <div id="test" class="test" :class="{show: scrolled}">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+       <section class="section">
+        <h2 class="h2">Почему мы?</h2>
+        <ul class="ul_section">
+            <li class="li_section"><h3 class="h3">В нашем парке только иномарки</h3></li>
+            <li class="li_section"><h3 class="h3">Медицинский контроль водителей перед выходом на линию</h3></li>
+            <li class="li_section"><h3 class="h3">Ежедневный технический контроль автомобиля</h3></li>
+            <li class="li_section"><h3 class="h3">Заключение договора в течении часа</h3></li>
+            <li class="li_section"><h3 class="h3">Прозрачная система оплаты</h3></li>
+            <li class="li_section"><h3 class="h3">Доставим в любой город России</h3></li>
+        </ul>
+       </section>
     </div>
 </template>
 
@@ -26,8 +42,9 @@
 export default{
     data(){
         return{
-        isActive:false,
-        isActive2:true
+        isActive:true,
+        isActive2:false,
+        scrolled:false,
         }
     },
     methods:{
@@ -35,8 +52,12 @@ export default{
             this.isActive = !this.isActive;
             this.isActive2 = !this.isActive2
         }
-    }
+            
+    },
+    mounted(){
 
+            
+}
 }
 
 </script>
@@ -45,11 +66,21 @@ export default{
 
 #generalHead{
     margin-top: -5vw;
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+}
+.content{
+    padding-top: 11vw;
+}
+.header{
+    height:100vh;
     background:no-repeat center url(../assets/bg.png);
     background-size: cover;
-    padding-top: 11vw;
+}
+.test{
+    display: none;
+}
+.show{
+    display: block;
 }
 .form{
     width: 25vw;
@@ -58,6 +89,20 @@ export default{
     backdrop-filter: blur(5px);
     border: solid 1px #9d9d9d;
     border-radius: .5vw;
+    transition: all ease 0.5s;
+}
+input:-webkit-autofill,
+input:-webkit-autofill:hover, 
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+    -webkit-text-fill-color: #FFFFFF;
+    -webkit-box-shadow:none;
+    transition: background-color 5000s ease-in-out 0s;
 }
 .input,.textarea{
     width: 85%;
@@ -67,10 +112,19 @@ export default{
     background: none;
     border: none;
     border-bottom: solid 1px #9d9d9d;
+    color: #FFFFFF;
+    padding: 0.5vw;
+    outline:none;
 }
 .textarea{
     height: 5vw;
     color: #cecece; 
+}
+.input.active{
+    opacity: 0;
+}
+textarea.active{
+    opacity: 0;
 }
 .btn{
     width: 50%;
@@ -80,6 +134,12 @@ export default{
     color: #FFFFFF;
     padding: .4vw 0;
     margin-left: 43%;
+    cursor: pointer;
+    transition: all ease-in-out 0.3s;
+}
+.btn:hover{
+    background-color: #FFFFFF;
+    color: #1d1d1d;
 }
 .tabs{
     position: relative;
@@ -119,5 +179,43 @@ export default{
 }
 ::placeholder{
     color:#cecece;
+}
+.get{
+    color: #FFFFFF;
+    width: 35vw;
+    font-weight: lighter;
+}
+.h1,.h4,.p{
+    font-weight: lighter;
+}
+.h1{
+    font-size: 5vw;
+    line-height: 5vw;
+}
+.h4{
+    font-size: 1.5vw;
+    margin: 4vw 0;
+    width: 30vw;
+}
+.takeCar{
+    font-size: 1.5vw;
+    border: none;
+    background-color: #007B1B;
+    border-radius: 5vw;
+    color: #FFFFFF;
+    width: 60%;
+    padding: 0.5vw 0 0.8vw 0;
+    cursor: pointer;
+}
+.section{
+    padding-left: 5vw;
+margin-bottom: 5vw;
+}
+.li_section{
+    font-size: 1.5vw;
+}
+.h2{
+    font-size: 2.5vw;
+    margin-bottom: 2vw;
 }
 </style>
